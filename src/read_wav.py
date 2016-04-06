0e9f2fd06054a24e32f0f7af904b2f2de57be854
Karthik <karthik@localhost>
2016-04-06 11:23:51 +0530

    Add tensorflow rnn

---
+++ src/read_wav.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @Author: Karthik
+# @Date:   2016-04-05 19:04:03
+# @Last Modified by:   Karthik
+# @Last Modified time: 2016-04-06 11:03:44
+
+from scipy.io import wavfile
+# import numpy
+# numpy.set_printoptions(threshold=numpy.nan)
+
+r, d = wavfile.read("../data/natabhairavi_violin.wav")
+right_stream = d[:,1]
+print ' '.join(map(str, list(right_stream)[10000:20000]))
+wavfile.write("../output/test.wav",  r, right_stream)
