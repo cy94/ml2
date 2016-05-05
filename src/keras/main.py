58d6b70cf165e57682f380d60bf58a445c30a678
Chandan Yeshwanth <chandan.yeshwanth@gmail.com>
2016-05-05 17:56:35 +0530

    convert wav samples to training data (X, Y) for RNN

--- src/keras/main.py
+++ src/keras/main.py
@@ -2,18 +2,53 @@
 # @Author: Chandan Yeshwanth
 # @Date:   2016-05-01 16:33:49
 # @Last Modified by:   Chandan Yeshwanth
-# @Last Modified time: 2016-05-03 22:01:30
+# @Last Modified time: 2016-05-05 17:56:14
 
 import audio as aud
 
 import numpy as np
 import matplotlib.pyplot as plt
 
+from scipy.signal import resample, decimate
+
+from keras.models import Sequential
 
 DEFAULT_RATE = 44100
 
 def roundup(x, up=100):
 	return x if x % up == 0 else x + up - x % up
+
+def quantize(signal, partitions, codebook):
+	""" http://stackoverflow.com/questions/15983986/convert-quantiz-function-to-python """
+	indices = []
+	quanta = []
+	for datum in signal:
+		index = 0
+		while index < len(partitions) and datum > partitions[index]:
+		    index += 1
+		indices.append(index)
+		quanta.append(codebook[index])
+	return np.array(quanta)
+
+def get_data_labels(wav_data, seq_len=50, shift=5):
+	""" create sequences of seq_len with a shift of shift values"""
+
+	new_data = np.copy(wav_data)
+	new_data -= int(new_data.mean())
+
+	result = []
+
+	for index in range(0, len(new_data) - seq_len, shift):
+		result.append(new_data[index: index + seq_len])
+
+
+	data = np.array(result)
+	# input - all but the last column 
+	X = data[:, :-1]
+	# output - last column 
+	Y = data[:,  -1]
+
+	return X, Y
 
 def main():
 	rate, data = aud.get_wav("violin.wav")
@@ -26,21 +61,30 @@ def main():
 	print "Cut:", new_data.size
 	aud.save_wav(new_data, "violin_cut.wav")
 
-	THRESHOLD = 8000
+	# plt.hist(new_data)
+	# plt.show()
 
-	new_data = np.vectorize(roundup)(new_data)
+	X, Y = get_data_labels(new_data)
+	generated = generate_audio(X, Y)
 
-	plt.hist(new_data)
-	plt.show()
-	# interval = 0.0001
+	aud.save_wav(new_data, "violin_cut.wav")
 
-	# new_data = aud.average_wav(new_data, interval)
-	# print "Averaged:", new_data.size
+def generate_audio(X, Y):
+	model = Sequential()
 
-	# new_data = aud.expand_wav(new_data, DEFAULT_RATE * interval)
-	# print "Expanded:", new_data.size
+	# add layers
+	# model.add(LSTM(32, stateful=True))
+	# model.add(Dropout(0.2))
+	# model.add(Activation('sigmoid'))
 
-	aud.save_wav(new_data, "violin_new.wav")
+	# compile model
+
+	# train
+	# model.fit(data, labels)
+
+	# generate
+	
+	return np.array(0)
 
 if __name__ == '__main__':
 	main()
--- src/keras/rnn.py
+++ src/keras/rnn.py
@@ -2,4 +2,7 @@
 # @Author: Chandan Yeshwanth
 # @Date:   2016-05-01 16:37:46
 # @Last Modified by:   Chandan Yeshwanth
-# @Last Modified time: 2016-05-01 16:37:46
+# @Last Modified time: 2016-05-05 12:08:14
+
+
+	
